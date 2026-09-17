6efca3077e3c2532f6d920a512267a75de31392f
Razzjel <astonodane@gmail.com>
2026-09-17 16:15:55 +0200

    refactor(fixtures): introduce custom test fixture for register page

---
+++ src/fixtures/test.fixture.ts
@@ -0,0 +1,16 @@
+import { test as base, expect} from '@playwright/test'
+import { RegisterPage } from "../pages/register.page"
+
+type PageFixtures = {
+  registerPage: RegisterPage;
+};
+
+export const test = base.extend<PageFixtures>({
+  registerPage: async ({ page }, use) => {
+    const registerPage = new RegisterPage(page);
+
+    await use(registerPage);
+  }
+})
+
+export { expect };
--- tests/e2e/auth.spec.ts
+++ tests/e2e/auth.spec.ts
@@ -1,11 +1,9 @@
-import { test, expect, Page } from '@playwright/test'
+import { test, expect } from '../../src/fixtures/test.fixture'
 import { RegisterPage } from '../../src/pages/register.page';
 import { generateUser } from '../../src/utils/user-factory';
 
 test.describe('Authentication', () => {
-  test('should register a new user successfully', async ({ page }) => {
-    const registerPage = new RegisterPage(page);
-
+  test('should register a new user successfully', async ({ registerPage, page }) => {
     const user = generateUser();
 
     await registerPage.goto();
